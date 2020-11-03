649f1777851d7a358be1f39cf40673a20959c571
shining-stars <43920879+shining-stars@users.noreply.github.com>
2020-11-03 21:12:40 +0800

    Update SmartPtr.cpp

--- SmartPointer/SmartPtr.cpp
+++ SmartPointer/SmartPtr.cpp
@@ -22,6 +22,7 @@ private:
 class RefPtr    //辅助类
 {   //该类成员访问权限全部为private，因为不想让用户直接使用该类
     friend class SmartPtr;  //定义智能指针类为友元，因为智能指针类需要直接操纵辅助类
+    RefPtr() : p(nullptr), count(1) {}
     RefPtr(Point *ptr) : p(ptr), count(1) { cout << "count = 1" <<endl; }
     RefPtr(int x, int y) : p(new Point(x, y)), count(1) { cout << "count = 1" <<endl; }
     ~RefPtr() { delete p; }
@@ -32,7 +33,7 @@ class RefPtr    //辅助类
 class SmartPtr  //智能指针类
 {
 public:
-    SmartPtr() = default;
+    SmartPtr() : rp(new RefPtr()) {}
     explicit SmartPtr(Point *ptr) : rp(new RefPtr(ptr)) { }    //构造函数（禁止隐式类型转换）
     SmartPtr(int x, int y) : rp(new RefPtr(x, y)) { }    //构造函数
     SmartPtr(const SmartPtr &sp) : rp(sp.rp) { ++rp->count; cout << "rp->count = " << rp->count <<endl; } //复制构造函数
@@ -42,6 +43,7 @@ public:
         cout << "rp->count = " << rp->count-1 <<endl;
         if(--rp->count == 0) {    //然后将引用计数减1，可以应对自赋值
             delete rp;
+            cout << "The destructor is called." <<endl;
         }
         rp = rhs.rp;
         return *this;
@@ -52,8 +54,10 @@ public:
     }
     ~SmartPtr() {   //析构函数
         cout << "rp->count = " << rp->count-1 <<endl;
-        if(--rp->count == 0)    //当引用计数减为0时，删除辅助类对象指针，从而删除基础对象
+        if(--rp->count == 0) {    //当引用计数减为0时，删除辅助类对象指针，从而删除基础对象
             delete rp;
+            cout << "The destructor is called." <<endl;
+        }
     }
 private:
     RefPtr *rp; //辅助类对象指针
@@ -64,7 +68,8 @@ int main()
     // SmartPtr sp0 = new Point(10, 8);    // Error! 禁止隐式类型转换（不能把普通指针隐式转换为智能指针）
     // SmartPtr sp0(new Point(10, 8));  // 用 普通指针 初始化 智能指针
     SmartPtr sp1 = SmartPtr::make_shared(10, 8);
-    SmartPtr sp2(sp1);
+    SmartPtr sp2;
+    sp2 = sp1;
     SmartPtr sp3(new Point(5, 5));
     sp3 = sp1;
     return 0;
